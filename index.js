39d0a3817625cd0261324e628bcf6702499fe881
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-03-03 02:53:40 -0500

    added array questions for user input and function to write README file

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 // TODO: Include packages needed for this application
 const generateMarkdown=require("./utils/generateMarkdown")
+const fs = require("fs")
 
 const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    
+
+    type: 'input', 
+    message: 'What is your project title?',
+    name: 'title'
+},
+{
+    type: 'list', 
+    message: 'What license will you be using?',
+    name: 'license',
+    choices: ["MIT", "Apache", "IBM", "No License"]
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const content = generateMarkdown(data)
+    fs.writeFile(fileName, content, err=>{
+        if(err){
+            console.error(err)
+            return
+        }
+        console.log("Sucess!")
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions) 
+    .then(data=>{
+        writeToFile("./output/README.md", data)
+    })
+}
 
 // Function call to initialize app
 init();
